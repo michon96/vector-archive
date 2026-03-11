80efadcb1303b41e103e1a76eeb14446a2cc7f96
Michon Bojador <mbojador@gmail.com>
2026-03-12 00:10:55 +0400

    Implemented Sound Functions

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField] AudioSource bgm;
+    [SerializeField] AudioSource sfx;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bgm.Play();
+        bgm.loop = true;
+        GameManager.Instance.OnAnswerCorrect += OnAnswerCorrect;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        GameManager.Instance.OnAnswerCorrect -= OnAnswerCorrect;
     }
+
+    private void OnAnswerCorrect(int obj)
+    {
+        sfx.Play();
+    }
+
+    
 }
